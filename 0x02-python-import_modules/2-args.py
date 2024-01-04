ae7f626c812462533a5859ef79de542c6c477462
Loïc Kami wsl <lkami@bogital.com>
2024-01-05 00:14:40 +0100

    add 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def main():
+    from sys import argv
+    argc = len(argv) - 1
+    print("{} argument{}{}"
+          .format(argc, "" if argc <= 1 else "s", ":" if argc > 0 else "."))
+
+    for i, arg in enumerate(argv[1:], start=1):
+        print("{}: {}".format(i, arg))
+
+
+if __name__ == "__main__":
+    main()
