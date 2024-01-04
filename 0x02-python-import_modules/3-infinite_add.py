6178dd491619060a2288ff31e72759c7b2fab42d
Loïc Kami wsl <lkami@bogital.com>
2024-01-05 00:38:20 +0100

    Add 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+def main():
+    from sys import argv
+    argc = len(argv) - 1
+    sum = 0
+    if argc > 0:
+        for i in range(1, argc + 1):
+            sum += int(argv[i])
+
+    print("{}".format(sum))
+
+
+if __name__ == "__main__":
+    main()
