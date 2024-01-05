2c20fa74a2a04b02161f8b7f0def2ea84cd5101e
Loïc Kami wsl <lkami@bogital.com>
2024-01-06 00:44:00 +0100

    Add 1st 5 tasks

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    length = len(my_list)
+    if (idx <= 0):
+        return None
+    if (idx > length - 1):
+        return None
+    return my_list[idx]
---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx <= 0 or idx > len(my_list):
+        return my_list.copy()
+    
+	# Create a new list with the updated element
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
