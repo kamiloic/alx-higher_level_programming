429548afbd8e28971aec96309a0f95030ac83913
Loïc Kami wsl <lkami@bogital.com>
2024-01-06 10:25:09 +0100

    1 update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    length = len(my_list)
-    if (idx <= 0):
-        return None
-    if (idx > length - 1):
+    if (idx < 0 and idx > len(my_list) - 1):
         return None
     return my_list[idx]
