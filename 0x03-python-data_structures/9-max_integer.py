e216ac6899a012f471af552f9555a53c358bf8d3
Loïc Kami wsl <lkami@bogital.com>
2024-01-06 02:58:25 +0100

    Add 7, 8, 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if (len(my_list) == 0):
+        return None
+
+    biggest_int = 0
+    for i in range(len(my_list)):
+        if (my_list[i] > biggest_int):
+            biggest_int = my_list[i]
+
+    return biggest_int
