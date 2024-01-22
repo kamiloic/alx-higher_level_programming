55912329040c0991cc956ccec0c3de402d0b2bd2
Loïc Kami wsl <lkami@bogital.com>
2024-01-23 00:36:21 +0100

    Add 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import stderr
+
+def safe_function(fct, *args):
+    try:
+        return fct(args[0], args[1])
+    except BaseException as e:
+          print("Exception: {}".format(e), file=stderr)
