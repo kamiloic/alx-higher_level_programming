bc5df575cf0a2bd10379caa6715eb6ef245000fc
Loïc Kami wsl <lkami@bogital.com>
2024-01-30 04:29:40 +0100

    Add say_my_name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module for printing names with optional last names.
+
+Methods:
+    say_my_name - Prints the provided name.
+"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints the provided name.
+
+    Parameters:
+    - first_name (str): First name.
+    - last_name (str, optional): Last name. Defaults to an empty string.
+
+    Raises:
+    - TypeError: If either first_name or last_name is not a string.
+
+    Example:
+    ::
+    
+        >>> say_my_name("John", "Doe")
+        My name is John Doe
+
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
