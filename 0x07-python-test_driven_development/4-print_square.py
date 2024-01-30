88fc47f701656cd4faaeff5659c1c1e59c6fe1ee
Loïc Kami wsl <lkami@bogital.com>
2024-01-30 04:30:07 +0100

    Add print_sqauare

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module for printing squares of a specific size using the '#' character.
+
+Methods:
+    print_square: - Prints a square of a specific size using the '#' character.
+
+"""
+
+
+def print_square(size):
+    """
+    Prints a square of a specific size using the '#' character.
+
+    Parameters:
+    - size (int): The size of the square.
+
+    Raises:
+    - TypeError: If the size is not an integer.
+    - ValueError: If the size is less than 0.
+    """
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
