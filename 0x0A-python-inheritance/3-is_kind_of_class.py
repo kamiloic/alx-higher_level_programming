bca17b592c3d0200fa50a714cec3218fbf5a9ca6
Loïc Kami wsl <lkami@bogital.com>
2024-02-08 03:32:33 +0100

    Add 3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for is_kind_of_class function."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if object is exactly an instance of the specified class."""
+    return isinstance(obj, a_class)
