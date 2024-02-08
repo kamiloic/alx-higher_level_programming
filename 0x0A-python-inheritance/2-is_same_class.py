a114194d317f599405300edd9d8d4478f322975c
Loïc Kami wsl <lkami@bogital.com>
2024-02-08 03:32:20 +0100

    Add 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for is_same_class function."""
+
+
+def is_same_class(obj, a_class):
+    """Inherits from list and adds a method to print a sorted list."""
+    return type(obj) == a_class
