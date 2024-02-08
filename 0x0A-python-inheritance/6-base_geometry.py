d719baf081a866564a76523dc0fc5a27e2398857
Loïc Kami wsl <lkami@bogital.com>
2024-02-08 03:33:01 +0100

    Add 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Empty class representing a base geometry."""
+
+    def area(self):
+        """Area method
+        Throw exception when not implemented
+        """
+        raise Exception("area() is not implemented")
