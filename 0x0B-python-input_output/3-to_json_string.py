e179b5d814ab5924c93d3f139be99764c50be938
Loïc Kami wsl <lkami@bogital.com>
2024-02-09 04:49:36 +0100

    Add 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""0x0B-python-input_output Module"""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object as a string."""
+    return json.dumps(my_obj)
