85c92bdbc859aa184aed731c4b6449b4666e104d
Loïc Kami <ruvice.kami@gmail.com>
2024-05-23 01:54:36 +0100

    Added 11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- Use database hbtn_0d_tvshows
+USE hbtn_0d_tvshows;
+
+-- Query to list all shows with their respective genre IDs (including NULL if no genre is linked)
+SELECT tv_shows.title, tv_show_genres.genre_id
+    FROM tv_shows
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+    ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
