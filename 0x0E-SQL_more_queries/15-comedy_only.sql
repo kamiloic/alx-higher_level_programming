c81e0a8712eef5821de41772dae312f855e657c5
Loïc Kami <ruvice.kami@gmail.com>
2024-05-23 01:55:11 +0100

    Added 15-comedy_only.sql

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,10 @@
+-- Use database hbtn_0d_tvshows
+USE hbtn_0d_tvshows;
+
+-- Query to list all Comedy shows
+SELECT tv_shows.title
+    FROM tv_shows
+    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+    ORDER BY tv_shows.title ASC;
