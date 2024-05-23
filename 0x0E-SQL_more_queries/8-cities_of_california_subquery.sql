cac3f1d06d2224590765ecf7456910c1c7794c20
Loïc Kami <ruvice.kami@gmail.com>
2024-05-23 01:56:48 +0100

    Added 8-cities_of_california_subquery.sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- Use database hbtn_0d_usa
+USE hbtn_0d_usa;
+
+-- Query to select cities of California using subquery
+SELECT * FROM cities
+    WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+    ORDER BY id ASC;
