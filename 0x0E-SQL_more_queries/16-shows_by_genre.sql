c44606b19b93f147e3573863336ab70e40248eef
Loïc Kami <ruvice.kami@gmail.com>
2024-05-23 01:55:23 +0100

    Added 16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Use database hbtn_0d_tvshows
+USE hbtn_0d_tvshows;
+
+-- Query to list all shows and their linked genres
+SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS name
+    FROM tv_shows
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    ORDER BY tv_shows.title ASC, tv_genres.name ASC;
